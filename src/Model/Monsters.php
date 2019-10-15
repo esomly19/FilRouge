7ab624117484d7c68e6534c890e6b7781749dbac
esomly19 <esomly19@gmail.com>
2019-10-15 09:03:12 +0200

    2 model personnage

--- src/Model/Monsters.php
+++ src/Model/Monsters.php
@@ -2,8 +2,13 @@
 namespace App\Model;
 use App\Database;
 class Monsters extends \Illuminate\Database\Eloquent\Model {
+    
     protected $table = 'ProjetFilRouge_Monstre';
+
+    use SoftDeletes;
+
     function battle(){
         return $this->hasMany('App\Model\Battles','id','monstre');
     }
+    
 }
