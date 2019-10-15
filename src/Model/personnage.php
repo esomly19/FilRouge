a07555656e2e8f1b286005c74347ee1d6cc9ea69
esomly19 <esomly19@github.com>
2019-10-15 09:26:47 +0200

    change

--- src/Model/personnage.php
+++ src/Model/personnage.php
@@ -5,6 +5,54 @@ namespace App\Model;
 use App\Database;
 
 class Personnage extends \Illuminate\Database\Eloquent\Model {
+    protected   $id,
+    $prenom,
+    $nom,
+    $poids,
+    $taille,
+    $vie,
+    $attaque,
+    $defense,
+    $agilite,
+    $photo,
+    $deleted_at;
+  
     protected $table = 'ProjetFilRouge_Personnage';
     public $timestamps = false;
+
+
+
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function getPrenom() {
+        return $this->prenom;
+    }
+    public function getNom() {
+        return $this->nom;
+    }
+    public function getTaille() {
+        return $this->taille;
+    }
+    public function getVie() {
+        return $this->Vie;
+    }
+    
+    public function getDegats() {
+        return $this->degats;
+    }
+    
+    public function getTimeToBeAsleep() {
+        return $this->timeToBeAsleep;
+    }
+    
+    public function getType() {
+        return $this->type;
+    }
+    
+    public function getAtout() {
+        return $this->atout;
+    }
+    
 }
