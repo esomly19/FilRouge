561fe4b43151e561315459f21554e1f723b14858
esomly19 <esomly19@github.com>
2019-10-15 10:05:33 +0200

    changement model personnage

--- src/Model/personnage.php
+++ src/Model/personnage.php
@@ -43,20 +43,32 @@ class Personnage extends \Illuminate\Database\Eloquent\Model {
         return $this->Vie;
     }
     
-    public function getDegats() {
-        return $this->degats;
+    public function getAttaque() {
+        return $this->attaque;
     }
     
-    public function getTimeToBeAsleep() {
-        return $this->timeToBeAsleep;
+    public function getDefense() {
+        return $this->defense;
     }
     
-    public function getType() {
-        return $this->type;
+    public function getAgilite() {
+        return $this->agilite;
+    }
+    public function getPhoto() {
+        return $this->photo;
+    }
+    public function setNom($nom) {
+        if (is_string($nom)) {     // Vérification si présence d'une chaîne de caractères
+            $this->nom = $nom;    // On assigne alors la valeur $nom à l'attribut _nom
+        }
     }
     
-    public function getAtout() {
-        return $this->atout;
+    public function setDegats($degats) {
+        $degats = (int)$degats; // Conversion de l'argument en nombre entier
+        // Vérification - Le nombre doit être strictemeznt positif et compris entre 0 et 100
+        if ($degats >= 0 && $degats <= 100) {
+            $this->degats = $degats; // on assigne alors la valeur $degats à l'attribut _degats
+        }
     }
     
 }
